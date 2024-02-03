a4079206aed5585fc79fcfefbac19abcaa67f3c5
haobingo <15879624465@sina.cn>
2024-02-03 16:36:54 +0800

    项目初始化

---
+++ haobingo_start/src/main/java/space/haobingo/haobingoSpringBoot3_Application.java
@@ -0,0 +1,16 @@
+package space.haobingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//@SpringBootApplication 来标注一个主程序类
+//说明这是一个Spring Boot应用
+@SpringBootApplication
+public class haobingoSpringBoot3_Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(haobingoSpringBoot3_Application.class, args);
+    }
+
+}
